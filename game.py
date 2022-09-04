8883049e9c536c3102b5fa75ae2f67c3653578d7
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2022-09-04 23:39:55 +0530

    Game sort of works now

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import sys
-
+from csv import reader, writer
+from time import sleep
 import pygame
 from pygame.locals import *
 
 pygame.init()
-
+FNAME = 'score.csv'
 player1_score = 0
 player2_score = 0
 
@@ -32,14 +33,42 @@ p1_scores = font_m.render("Player 1 scores !", True, red)
 p2_scores = font_m.render("Player 2 scores !", True, red)
 
 # Importing images for players, football, field
-c1 = pygame.image.load("P1.png").convert()
-c2 = pygame.image.load("P2.png").convert()
+c1 = pygame.image.load("righty.png")
+c2 = pygame.image.load("lefty.png")
 ball = pygame.image.load("football.png")
 background = pygame.image.load("football field.jpg").convert()
 goal1 = pygame.image.load("post.jpg").convert()
 goal2 = pygame.image.load("post2.jpg").convert()
-ground.blit(ball, [0, 0])
 ground.blit(background, [0, 0])
+ft1 = font_m.render('Play', True, red)
+ft2 = font_m.render('View history', True, red)
+ground.blit(ft1, [100, 50])
+ground.blit(ft2, [250, 50])
+k, sh = False, False
+while True:
+    for action in pygame.event.get():
+        if action.type == pygame.MOUSEBUTTONDOWN:
+            # print(mouse)
+            if 150 > mouse[0] > 50 and 110 > mouse[1] > 54:
+                k = True
+            if 520 > mouse[0] > 250 and 110 > mouse[1] > 54:
+                sh = True
+    if k:
+        break
+    if sh:
+        with open(FNAME, 'r') as f1:
+            csv_r = list(reader(f1))
+            str_tbp = ""
+            off = 40
+            for i in range(len(csv_r)):
+                print('dog')
+                fsr = font_s.render(f"Player {csv_r[i][0]} won", True, red)
+                ground.blit(fsr, [300, 200 + (i * off)])
+        sh = False
+    mouse = pygame.mouse.get_pos()
+    pygame.display.update()
+
+ground.blit(ball, [0, 0])
 ground.blit(c1, [0, 0])
 ground.blit(c2, [0, 0])
 ground.blit(goal1, [0, 0])
@@ -56,7 +85,7 @@ br = background.get_rect()
 # Defining the starting point of the ball
 ball_area.left = 600
 ball_area.top = 350
-
+IFST = False
 # Starting point of player 1
 c1_area.left = 160
 c1_area.top = 280
@@ -110,19 +139,26 @@ def ball_exit():
 
 
 def ball_collision():
+    global IFST
     # player 1 and ball collision
-    if c1_area.colliderect(ball_area):
+    if c1_area.colliderect(ball_area) and not IFST:
         if c1_area.colliderect(ball_area.move(-speed[0], 0)):
             speed[1] = -speed[1]
+
         if c1_area.colliderect(ball_area.move(0, speed[1])):
             speed[0] = -speed[0]
-
+        IFST = True
+        sleep(0.00025)
+        IFST = False
     # player 2 and ball collision
-    if c2_area.colliderect(ball_area):
+    if c2_area.colliderect(ball_area) and not IFST:
         if c2_area.colliderect(ball_area.move(-speed[0], 0)):
             speed[1] = -speed[1]
         if c2_area.colliderect(ball_area.move(0, speed[1])):
             speed[0] = -speed[0]
+        IFST = True
+        sleep(0.00025)
+        IFST = False
 
 
 def goal():
@@ -172,6 +208,12 @@ def playgame():
         if g2_area.colliderect(ball_area):
             player1_score += 1
             ground.blit(p1_scores, [450, 190])
+            ball_area.left = 500
+            ball_area.top = 280
+            c1_area.left = 160
+            c1_area.top = 280
+            c2_area.left = 1033
+            c2_area.top = 280
             pygame.display.update()
             pygame.time.wait(2000)
 
@@ -179,6 +221,12 @@ def playgame():
         if g1_area.colliderect(ball_area):
             player2_score += 1
             ground.blit(p2_scores, [450, 190])
+            ball_area.left = 500
+            ball_area.top = 280
+            c1_area.left = 160
+            c1_area.top = 280
+            c2_area.left = 1033
+            c2_area.top = 280
             pygame.display.update()
             pygame.time.wait(2000)
 
@@ -203,13 +251,19 @@ def playgame():
                 if action.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-            if (player1_score >= 3):
-                pygame.quit()
-                sys.exit()
+        if player1_score >= 3:
+            with open(FNAME, 'a', newline='') as f2:
+                csv_w = writer(f2)
+                csv_w.writerow([1, ])
+            pygame.quit()
+            sys.exit()
 
-            if (player2_score >= 3):
-                pygame.quit()
-                sys.exit()
+        if (player2_score >= 3):
+            with open(FNAME, 'a', newline='') as f2:
+                csv_w = writer(f2)
+                csv_w.writerow([2, ])
+            pygame.quit()
+            sys.exit()
 
         # Updating background
         ground.blit(background, [0, 0])
